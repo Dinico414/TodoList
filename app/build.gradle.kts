b5f5cd16b37359835feb675aea5e1eecc5402ac4
Dinico414 <116182930+Dinico414@users.noreply.github.com>
2023-10-27 17:48:09 +0200

    add main activity, update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "todolist.xenon"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "todolist.xenon"
         minSdk = 33
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
---
+++ app/src/main/java/com.todolist.xenon/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.todolist.xenon
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import todolist.xenon.R
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
