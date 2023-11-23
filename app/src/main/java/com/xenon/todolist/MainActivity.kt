25558680dea01fd1860ddce7ffe7fb947750c8f1
Dinico414 <116182930+Dinico414@users.noreply.github.com>
2023-11-23 05:22:57 +0100

    Added placeholder if no Task

--- app/src/main/java/com/xenon/todolist/MainActivity.kt
+++ app/src/main/java/com/xenon/todolist/MainActivity.kt
@@ -1,45 +1,60 @@
 package com.xenon.todolist
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.xenon.todolist.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), TaskItemClickListener
-{
+class MainActivity : AppCompatActivity(), TaskItemClickListener {
     private lateinit var binding: ActivityMainBinding
     private lateinit var taskViewModel: TaskViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Move view initialization here
+        val todoListRecycleView: RecyclerView = findViewById(R.id.todoListRecycleView)
+        val noTasksTextView: TextView = findViewById(R.id.noTasks)
+
         taskViewModel = ViewModelProvider(this)[TaskViewModel::class.java]
+
         binding.NewTaskButton.setOnClickListener {
             NewTaskSheet(null).show(supportFragmentManager, "newTaskTag")
         }
-        setRecycleView()
+
+        setRecyclerView()
+
+        // Rest of your code
     }
-    private fun setRecycleView()
-    {
+
+    private fun setRecyclerView() {
         val mainActivity = this
-        taskViewModel.taskItems.observe(this)
-        {
+        taskViewModel.taskItems.observe(this) { taskItems ->
             binding.todoListRecycleView.apply {
                 layoutManager = LinearLayoutManager(applicationContext)
-                adapter = it?.let { it1 -> TaskItemAdapter(it1, mainActivity) }
+                adapter = taskItems?.let { TaskItemAdapter(it, mainActivity) }
+            }
+
+            // Update visibility based on the item count
+            if (taskItems?.isEmpty() == true) {
+                binding.noTasks.visibility = View.VISIBLE
+            } else {
+                binding.noTasks.visibility = View.GONE
             }
         }
     }
 
-    override fun editTaskItem(taskItem: TaskItem)
-    {
-       NewTaskSheet(taskItem).show(supportFragmentManager,"newTaskTag")
+    override fun editTaskItem(taskItem: TaskItem) {
+        NewTaskSheet(taskItem).show(supportFragmentManager, "newTaskTag")
     }
 
-    override fun completeTaskItem(taskItem: TaskItem)
-    {
-
+    override fun completeTaskItem(taskItem: TaskItem) {
         taskViewModel.toggleCompleted(taskItem)
     }
 }
