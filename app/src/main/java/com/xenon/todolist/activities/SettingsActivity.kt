74695f5b0ed8df0c7d8d5e46efeb073d3d5897a4
Dinico414 <116182930+Dinico414@users.noreply.github.com>
2024-01-27 19:00:16 +0100

    add dark mode switch dark light system

--- app/src/main/java/com/xenon/todolist/activities/SettingsActivity.kt
+++ app/src/main/java/com/xenon/todolist/activities/SettingsActivity.kt
@@ -4,14 +4,17 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.provider.Settings
-import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.xenon.todolist.SharedPreferenceManager
 import com.xenon.todolist.databinding.ActivitySettingsBinding
 import java.util.Locale
 
 class SettingsActivity : BaseActivity() {
     private lateinit var binding: ActivitySettingsBinding
+    private val themeTitleList = arrayOf("Light", "Dark", "System")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +22,28 @@ class SettingsActivity : BaseActivity() {
         setContentView(binding.root)
         adjustBottomMargin(binding.layoutMain)
         setupViews()
+
+        val themeSelectionValue = findViewById<TextView>(com.xenon.todolist.R.id.theme_selection_value)
+
+        val sharedPreferenceManager = SharedPreferenceManager(this)
+        var checkedTheme = sharedPreferenceManager.theme
+        themeSelectionValue.text = themeTitleList[sharedPreferenceManager.theme]
+
+        val themeDialog = MaterialAlertDialogBuilder(this)
+            .setTitle("Select Theme")
+            .setPositiveButton("OK") { _, _ ->
+                sharedPreferenceManager.theme = checkedTheme
+                AppCompatDelegate.setDefaultNightMode(sharedPreferenceManager.themeFlag[checkedTheme])
+                themeSelectionValue.text = themeTitleList[checkedTheme]
+            }
+            .setSingleChoiceItems(themeTitleList, checkedTheme) { _, which ->
+                checkedTheme = which
+            }
+            .setCancelable(false)
+
+        binding.themeSelectionHolder?.setOnClickListener {
+            themeDialog.show()
+        }
     }
 
     private fun setupViews() {
@@ -26,8 +51,7 @@ class SettingsActivity : BaseActivity() {
         binding.languageSelectionHolder.setOnClickListener {
             val intent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
                 Intent(Settings.ACTION_APP_LOCALE_SETTINGS)
-            }
-            else {
+            } else {
                 Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
             }
             intent.data = Uri.fromParts("package", packageName, null)
