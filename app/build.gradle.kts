43859967a1e9ac6ffd182fcd728ae613894d9bfa
Dinico414 <116182930+Dinico414@users.noreply.github.com>
2025-04-23 11:58:32 +0200

    upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 29
         targetSdk = 34
         versionCode = 5
-        versionName = "1.5.0"
+        versionName = "1.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
